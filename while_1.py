996c464f88ae87919e9a1c043e2cb67698e47c0e
Andre Apriyana <47628631+dre03@users.noreply.github.com>
2024-11-01 20:35:38 +0700

    tugas ke 5

---
+++ while_1.py
@@ -0,0 +1,15 @@
+
+# mencetak 1-5
+# count = 1
+# while count <= 5:
+#     print(count)
+#     count += 1
+
+j = 1
+while(j<=10):
+    k = 1
+    while(k<=10):
+        print(j*k, end=" ")
+        k+=1
+    print()
+    j+=1
